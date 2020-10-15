fad1fc7398668ca8a7a4ef489845a4f7c53e7a8d
Taufik Hidayat <projectdts2020@gmail.com>
2020-10-15 14:52:58 +0700

    init get config

---
+++ menu-service/config/config.go
@@ -0,0 +1,5 @@
+package config
+
+type config struct {
+	Port string
+}
--- menu-service/main.go
+++ menu-service/main.go
@@ -6,14 +6,41 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
+	"github.com/taufikhidayatugmbe03/pengenalan-microservice/menu-service/config"
 	"github.com/taufikhidayatugmbe03/pengenalan-microservice/menu-service/handler"
 )
 
 func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
 	router := mux.NewRouter()
 
 	router.Handle("/add-product", http.HandlerFunc(handler.AddMenuHandler))
 
 	fmt.Println("Server listen on :8000")
 	log.Panic(http.ListenAndServe(":8000", router))
+	fmt.Printf("Server listen on :%s", cfg.Port)
+	log.Panic(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), router))
+}
+
+func getConfig() (config.config, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config.yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
 }
