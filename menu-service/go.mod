d883ebf694d0bde62181c55c53fadbe1a8db1f0d
Taufik Hidayat <projectdts2020@gmail.com>
2020-10-15 14:31:42 +0700

    using utils wrapper

--- menu-service/go.mod
+++ menu-service/go.mod
@@ -1,8 +1,9 @@
-module github.com/taufikhidayat/pengenalan-microservice
+module github.com/taufikhidayatugmbe03/pengenalan-microservice/menu-service
 
 go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/taufikhidayatugmbe03/pengenalan-microservice v0.0.0-20201015065620-5970f9e12806
 	gorm.io/gorm v1.20.2 // indirect
 )
--- menu-service/handler/menu.go
+++ menu-service/handler/menu.go
@@ -1,21 +1,12 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"github.com/taufikhidayatugmbe03/pengenalan-microservice/utils"
 )
 
 // AddMenuHandler handle add menu
 func AddMenuHandler(w http.ResponseWriter, r *http.Request) {
-	response, err := json.Marshal(map[string]interface{}{
-		"success": true,
-	})
-
-	if err != nil {
-		fmt.Printf("Failed generate response %s", err)
-		return
-	}
-
-	w.Write(response)
+	utils.WrapAPISuccess(w, r, "success", 200)
 }
